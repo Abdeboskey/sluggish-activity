61f69369fdd6915ab3424b472fcc91a233255cd9
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-16 21:27:49 -0600

    Remove react fragment

--- src/components/ChalkSlug/ChalkSlug.js
+++ src/components/ChalkSlug/ChalkSlug.js
@@ -5,11 +5,9 @@ import classes from './ChalkSlug.module.scss'
 
 const ChalkSlug = () => {
   return (
-    <>
     <div className={classes.slugjug}>
       <img className={classes.ChalkSlug} src={chalk_slug} alt="a chalk slug" />
     </div>
-    </>
   );
 }
 
