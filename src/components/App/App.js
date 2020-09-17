fecd4ae6cf7dd0bf2d1be2dbe48b5559e199aea2
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-17 17:47:27 -0600

    Add textbubble component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ChalkSlug from '../ChalkSlug/ChalkSlug';
+import TextBubble from '../TextBubble/TextBubble'
 import classes from './App.module.scss';
 
 function App() {
   return (
     <main className={classes.App}>
+      <TextBubble />
       <ChalkSlug />
     </main>
   );
