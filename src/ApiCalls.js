d55717c7418caa1a00f8f377b60453860abad366
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-17 14:49:55 -0600

    Interpolate base activity #3

--- src/ApiCalls.js
+++ src/ApiCalls.js
@@ -3,12 +3,12 @@ const activityBase = 'http://www.boredapi.com/api/activity?'
 export const getActivity = (participants, type) => {
   const numPeople = participants ? `participants=${participants}&` : '';
   const activityType = type ? `type=${type}` : '';
-  fetch(`activityBase${numPeople}${activityType}`)
+  return fetch(`${activityBase}${numPeople}${activityType}`)
     .then(response => {
 			if (response.ok) {
 				return response.json()
 			} else {
 				throw response
 			}
-		})
+		});
 }
