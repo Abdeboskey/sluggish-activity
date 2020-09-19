5d147ca09563c493a7bf54c2ce63eb8556d94583
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-18 22:42:10 -0600

    Create dropdown options for multiple people #6

--- src/components/ActivityType/ActivityType.js
+++ src/components/ActivityType/ActivityType.js
@@ -1,28 +1,47 @@
 import React from 'react'
 
-const ActivityType = ({ type, selectType }) => {
+const ActivityType = ({ solo, withOthers, type, selectType }) => {
   return (
     <>
-      <label htmlFor="type">
-        Type of Activity:
-        <select
-          name="type"
-          value={type}
-          onChange={(event) => selectType(event)}
-        >
-          <option value="">Please choose one</option>
-          <option value="any">Any</option>
-          <option value="education">Education</option>
-          <option value="recreational">Recreational</option>
-          <option value="social">Social</option>
-          <option value="diy">DIY</option>
-          <option value="charity">Charity</option>
-          <option value="cooking">Cooking</option>
-          <option value="relaxation">Relaxation</option>
-          <option value="music">Music</option>
-          <option value="busywork">Busywork</option>
-        </select>
-      </label>
+      {solo &&
+        <label htmlFor="type">
+          Type of Activity:
+          <select
+            name="type"
+            value={type}
+            onChange={(event) => selectType(event)}
+          >
+            <option value="">Please choose one</option>
+            <option value="any">Any</option>
+            <option value="education">Education</option>
+            <option value="recreational">Recreational</option>
+            <option value="social">Social</option>
+            <option value="diy">DIY</option>
+            <option value="charity">Charity</option>
+            <option value="cooking">Cooking</option>
+            <option value="relaxation">Relaxation</option>
+            <option value="music">Music</option>
+            <option value="busywork">Busywork</option>
+          </select>
+        </label>
+      }
+      {withOthers &&
+        <label htmlFor="type">
+          Type of Activity:
+          <select
+            name="type"
+            value={type}
+            onChange={(event) => selectType(event)}
+          >
+            <option value="">Please choose one</option>
+            <option value="any">Any</option>
+            <option value="recreational">Recreational</option>
+            <option value="social">Social</option>
+            <option value="cooking">Cooking</option>
+            <option value="music">Music</option>
+          </select>
+        </label>
+      }
       <br />
     </>
   )
