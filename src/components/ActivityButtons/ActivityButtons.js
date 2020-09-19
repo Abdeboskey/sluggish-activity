00a1dbdcacc01f419a87729c3b9ebe5f4920fc8f
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-18 22:56:03 -0600

    Add style to buttons

--- src/components/ActivityButtons/ActivityButtons.js
+++ src/components/ActivityButtons/ActivityButtons.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import classes from './ActivityButtons.module.scss'
 
 const ActivityButtons = ({ activity, suggestActivity, startOver }) => {
   return (
     <>
       <h3>What if you {activity.toLowerCase()}?</h3>
-      <button>Thanks! I'll try that.</button>
-      {activity.includes("stargazing") && (
-        <button>Can we do that together?</button>
-      )}
-      <button onClick={suggestActivity}>Can you suggest something else?</button>
-      <button onClick={startOver}>Can I start over?</button>
+      <div className={classes.btnBar}>
+        <button className={classes.activityBtn}>Thanks! I'll try that.</button>
+        {activity.includes("stargazing") && (
+          <button className={classes.activityBtn}>
+            Can we do that together?
+          </button>
+        )}
+        <button className={classes.activityBtn} onClick={suggestActivity}>
+          Can you suggest something else?
+        </button>
+        <button className={classes.activityBtn} onClick={startOver}>
+          Can I start over?
+        </button>
+      </div>
     </>
-  )
+  );
 }
 
 export default ActivityButtons
