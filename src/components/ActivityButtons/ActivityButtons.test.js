07c121dec8ebfb421206ce6066b615e9396d4135
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-20 14:43:30 -0600

    Create implementation test for suggestActivity #22

--- src/components/ActivityButtons/ActivityButtons.test.js
+++ src/components/ActivityButtons/ActivityButtons.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ActivityButtons from './ActivityButtons'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
 
@@ -21,9 +21,28 @@ describe('ActivityButtons', () => {
     const saveButton = getByRole('button', { name: /thanks! i'll try that/i})
     const refreshButton = getByRole('button', { name: /something else?/i})
     const startOverButton = getByRole('button', { name: /start over?/i})
-    
+
     expect(saveButton).toBeInTheDocument()
     expect(refreshButton).toBeInTheDocument()
     expect(startOverButton).toBeInTheDocument()
+  })
+
+  it('should fire suggestActivity when the \'suggest something else\' button is clicked',  () => {
+    const mockSuggestActivity = jest.fn()
+    const startOver = jest.fn()
+    const { getByRole } = render(
+      <MemoryRouter>
+        <ActivityButtons
+          activity={"Learn to Yo-Yo"}
+          suggestActivity={mockSuggestActivity}
+          startOver={startOver}
+        />
+      </MemoryRouter>
+    )
+
+    const refreshButton = getByRole('button', { name: /something else/i})
+    fireEvent.click(refreshButton)
+
+    expect(mockSuggestActivity).toHaveBeenCalledTimes(1)
   })
 })
