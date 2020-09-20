28f57dae2afbcb48c166dfc70a344dbcc00fd096
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-20 16:16:28 -0600

    Create method to save current activity to localstorage #14

--- src/components/ActivityButtons/ActivityButtons.js
+++ src/components/ActivityButtons/ActivityButtons.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import classes from './ActivityButtons.module.scss'
 import PropTypes from 'prop-types'
+import { DateTime } from 'luxon'
 
 const ActivityButtons = ({ activity, link, suggestActivity, startOver }) => {
+  const saveActivity = () => {
+    const activityToSave = {
+      date: DateTime.local().toLocaleString(),
+      description: activity,
+      link: link || null,
+    }
+    
+    if (localStorage.getItem('savedActivities') === null) {
+      let activities = [activityToSave]
+      localStorage.setItem('savedActivities', JSON.stringify(activities))
+    } else {
+      const activities = JSON.parse(localStorage.getItem('savedActivities'))
+      activities.push(activityToSave)
+      localStorage.setItem('savedActivities', JSON.stringify(activities))
+    }
+  }
+
   return (
     <>
       <h3>What if you {activity[0].toLowerCase() + activity.substring(1)}?</h3>
       {link && <h4>Check it out <a href={link} target='_blank'>here!</a></h4>}
       <div className={classes.btnBar}>
-        <button className={classes.activityBtn}>Thanks! I'll try that.</button>
+        <button className={classes.activityBtn} onClick={saveActivity}>Thanks! I'll try that.</button>
         {activity.includes("stargazing") && (
           <button className={classes.activityBtn}>
             Can we do that together?
