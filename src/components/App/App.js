5443b1bcc4a4a7134bec68b9642c5b13bb24a9cf
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-21 16:56:35 -0600

    Create path for doesnotexist #30

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import ChalkSlug from '../ChalkSlug/ChalkSlug'
 import Home from '../Home/Home'
 import Journal from '../Journal/Journal'
 import TextBubble from '../TextBubble/TextBubble'
+import DoesNotExist from '../DoesNotExist/DoesNotExist'
 import classes from './App.module.scss'
 import sunnyField from '../../assets/sunnyField.jpg'
 
@@ -11,18 +12,23 @@ function App() {
   return (
     <main className={classes.App}>
       <img className={classes.background} src={sunnyField} alt="A Sunny Field with Butterflies"/>
-      <Route
-        exact path="/"
-        render={() => <Home />}
-      />
-      <Route
-        exact path="/activity"
-        render={() => <TextBubble />}
-      />
-      <Route
-        exact path="/journal"
-        render={()=> <Journal />}
-      />
+      <Switch>
+        <Route
+          exact path="/"
+          render={() => <Home />}
+        />
+        <Route
+          exact path="/activity"
+          render={() => <TextBubble />}
+        />
+        <Route
+          exact path="/journal"
+          render={() => <Journal />}
+        />
+        <Route 
+          render={() => <DoesNotExist/>}
+        />
+      </Switch>
       <ChalkSlug />
     </main>
   )
