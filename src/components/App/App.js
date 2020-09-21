1437bb593949852b9c399fa124c0f456c0a4b854
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-20 22:10:51 -0600

    Add route for journal component #21

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import ChalkSlug from '../ChalkSlug/ChalkSlug'
 import Home from '../Home/Home'
+import Journal from '../Journal/Journal'
 import TextBubble from '../TextBubble/TextBubble'
 import classes from './App.module.scss'
 
@@ -16,7 +17,10 @@ function App() {
         exact path="/activity"
         render={() => <TextBubble />}
       />
-      
+      <Route
+        exact path="/journal"
+        render={()=> <Journal />}
+      />
       <ChalkSlug />
     </main>
   )
