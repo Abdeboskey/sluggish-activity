c13c7e91af8a9cf0cb3e014104eba6824cf020c5
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-21 11:21:30 -0600

    Create unit test for home component #27

---
+++ src/components/Home/Home.test.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Home from './Home'
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+
+describe('Home', () => {
+  it('should display the correct elements when rendered', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <Home /> 
+      </MemoryRouter>
+    )
+
+    const welcomeMessage = getByRole('heading', { name: /do for you today/i })
+    const iCanDoMessage = getByRole('heading', { name: /i can help/i })
+    const activityButton = getByRole('button', { name: /find an activity/i })
+    const journalButton = getByRole('button', { name: /look at my journal/i })
+    
+    expect(welcomeMessage).toBeInTheDocument()
+    expect(iCanDoMessage).toBeInTheDocument()
+    expect(activityButton).toBeInTheDocument()
+    expect(journalButton).toBeInTheDocument()
+  })
+})
