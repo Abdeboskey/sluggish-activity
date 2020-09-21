31ac2cb724b0ce468932f842da33c2f1f78c7053
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-20 22:11:23 -0600

    Create links for different routes #24

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import classes from './Home.module.scss'
 
 const Home = () => {
-  return(
+  return (
     <section className={classes.HomeView}>
-      <h2>Oh, Hi Friend!</h2>
-      <h3>How Are You?</h3>
-      <button className={classes.answerBtn}>I'm well!</button>
-      <button className={classes.answerBtn}>I'm ok...</button>
-      <button className={classes.answerBtn}>Not so great.</button>
+      <h2>What can I do for you today, friend?</h2>
+      <div className={classes.btnBar}>
+        <Link to="/activity">
+          <button className={classes.answerBtn}>Find an Activity</button>
+        </Link>
+        <Link to="/journal">
+          <button className={classes.answerBtn}>Look at My Journal</button>
+        </Link>
+        {/* <Link>
+          <button className={classes.answerBtn}>Let's Go Stargazing</button>
+        </Link> */}
+      </div>
     </section>
-  )
+  );
 }
 
 export default Home
