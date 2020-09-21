ae7326f36e3c9585a79651fdec8f53fa97ebeb96
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-20 21:28:21 -0600

    Add initial routes #24

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import ChalkSlug from '../ChalkSlug/ChalkSlug'
+import Home from '../Home/Home'
 import TextBubble from '../TextBubble/TextBubble'
 import classes from './App.module.scss'
 
 function App() {
   return (
     <main className={classes.App}>
-      <TextBubble />
+      <Route
+        exact path="/"
+        render={() => <Home />}
+      />
+      <Route
+        exact path="/activity"
+        render={() => <TextBubble />}
+      />
+      
       <ChalkSlug />
     </main>
   )
