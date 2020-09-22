50d1abf007f45597e894095f0888570e4a9054bc
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-21 20:32:22 -0600

    Add classnames for styling

--- src/components/ActivityPrompts/ActivityPrompts.js
+++ src/components/ActivityPrompts/ActivityPrompts.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classes from './ActivityPrompts.module.scss'
 
 const ActivityPrompts = ({ solo, withOthers }) => {
   return (
     <>
       {!solo && !withOthers && (
-        <h3>
-          Would you like to do something by yourself<br />
+        <h3 className={classes.prompt}>
+          Would you like to do something by yourself
+          <br />
           or would you like some company?
         </h3>
       )}
       {(solo || withOthers) && (
-        <h3>What kind of activity would you like to do?</h3>
+        <h3 className={classes.prompt}>
+          What kind of activity would you like to do?
+        </h3>
       )}
     </>
-  )
+  );
 }
 
 export default ActivityPrompts
