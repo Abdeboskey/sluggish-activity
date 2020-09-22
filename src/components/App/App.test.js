81644c32a2e9aacef77ab67733b6a8f3da0ecb24
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-21 18:55:26 -0600

    Add tests for home, journal and activity routes #33

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,26 +1,51 @@
 import React from 'react'
 import App from './App'
-import { render } from '@testing-library/react'
+import { fireEvent, render } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import '@testing-library/jest-dom'
 
 describe('App', () => {
+
   it('Should display the home component when first rendered', () => {
-
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )    
+    
+    const homeHeading = getByRole('heading', { name: /what can i do for you today/i })
+    
+    expect(homeHeading).toBeInTheDocument()
   })
-
+  
   it('Should display the TextBubble when a user clicks "Find an Activity"', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )    
 
+    const activityButton = getByRole('button', { name: /find an activity/i })
+    fireEvent.click(activityButton)
+    
+    const activityPrompt = getByRole('heading', { name: /do something by yourself/i })
+
+    expect(activityPrompt).toBeInTheDocument()
   })
-
+  
   it('Should display the Journal when a user clicks "Look at My Journal"', () => {
+    const { getByRole } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    )    
 
+    const journalButton = getByRole('button', { name: /look at my journal/i })
+    fireEvent.click(journalButton)
+    
+    const activityLog = getByRole('heading', { name: /activities i\'ve tried/i })
+
+    expect(activityLog).toBeInTheDocument()
   })
-
-  it('Should display the home component when first rendered', () => {
-
-  })
-
 
 })
-
