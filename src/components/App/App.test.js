1c484ad0e6716bcd74cd8d2c3e70d61e4c110ca2
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-21 18:38:55 -0600

    Create placeholders for router testing #33

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,26 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import App from './App'
+import { render } from '@testing-library/react'
+import { MemoryRouter } from 'react-router-dom'
+import '@testing-library/jest-dom'
 
-test.skip('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('Should display the home component when first rendered', () => {
+
+  })
+
+  it('Should display the TextBubble when a user clicks "Find an Activity"', () => {
+
+  })
+
+  it('Should display the Journal when a user clicks "Look at My Journal"', () => {
+
+  })
+
+  it('Should display the home component when first rendered', () => {
+
+  })
+
+
+})
+
