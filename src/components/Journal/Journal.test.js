7f518db20583c9fdfe3dc280389c42c91cca2cfc
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2020-09-21 19:51:11 -0600

    Remove unused hooks and add comment about testing journal entry

--- src/components/Journal/Journal.test.js
+++ src/components/Journal/Journal.test.js
@@ -6,33 +6,8 @@ import '@testing-library/jest-dom'
 jest.mock('../../localStorageCalls.js')
 
 describe('Journal', () => {
-  // let activities
 
-  // beforeEach(() => {
-  //   global.localStorage.clear()
-  //   activities = [
-  //     {
-  //       date: "9/20/2020",
-  //       description: "Look at pictures and videos of cute animals",
-  //       link: null
-  //     },
-  //     {
-  //       date: "9/21/2020",
-  //       description: "Hire a clown for my birthday party",
-  //       link: null
-  //     },
-  //     {
-  //       date: "9/22/2020",
-  //       description: "Write a research paper about sharks",
-  //       link: "www.sharksider.com"
-  //     },
-  //   ]
-  //   global.localStorage.setItem('savedActivities', JSON.stringify(activities));
-  // })
-
-  // afterEach(() => {
-  //   global.localStorage.clear()
-  // })
+  // Saving an activity to the journal is tested in App.test.js
 
   it('should display the correct headings when rendered', () => {
     const { getByRole } = render(
@@ -59,22 +34,4 @@ describe('Journal', () => {
 
     expect(goHomeButton).toBeInTheDocument()
   })
-
-  // it.skip('should display a list of saved activities', async () => {
-  //   const { findByText } = render(
-  //     <MemoryRouter>
-  //       <Journal />
-  //     </MemoryRouter>
-  //   )
-
-  //   const firstEntryDate = await findByText(/9\/20\/2020 -/i)
-  //   const firstEntryDescription = await findByText(/look at pictures and videos of cute animals/i)
-  //   const lastEntryDate = await findByText(/9\/22\/2020 -/i)
-  //   const lastEntryDescription = await findByText(/write a research paper about sharks/i)
-    
-  //   expect(firstEntryDate).toBeInTheDocument()
-  //   expect(firstEntryDescription).toBeInTheDocument()
-  //   expect(lastEntryDate).toBeInTheDocument()
-  //   expect(lastEntryDescription).toBeInTheDocument()
-  // })
 })
